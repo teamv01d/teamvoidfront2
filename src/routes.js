0d142c7ae105eceaf1b2f81396f44345bd6d88cb
Damla Dalgıç <damlabileydi@gmail.com>
2021-07-28 16:54:25 +0300

    kurumsal giris ekraninin tasarimi yapildi.

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 import IndividualLogin from './views/IndividualLogin.vue'
-
+import CompanyLogin from './views/CompanyLogin.vue'
 export const routes = [
     {
         path: '/',
         name: 'IndividualLogin',
         component: IndividualLogin
+    },
+    {
+        path: '/company',
+        name: 'CompanyLogin',
+        component: CompanyLogin
     },
 ]
