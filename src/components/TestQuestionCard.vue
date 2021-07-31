95fa93a07ad99ef22f0c9033501c04a5813f74fe
Damla Dalgıç <damlabileydi@gmail.com>
2021-08-01 00:34:35 +0300

    Yetenek testi icin timer olusturuldu.

--- src/components/TestQuestionCard.vue
+++ src/components/TestQuestionCard.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <div max-width="1500">
+    <div>
       <p class="text-h6 r">Soru: 8/10</p>
     </div>
     <v-card max-width="1500">
       <v-row>
-        <v-col cols="8" offset="1">
+        <v-col cols="7" offset="1">
           <v-window v-model="step">
             <v-window-item :value="1">
               <P
@@ -27,12 +27,13 @@
               </div>
 
               <v-row>
-                <v-col
+                <v-col cols="2"
                   ><v-btn color="#FFFFFF" elevation="0">
                     <v-icon x-large> mdi-arrow-left-thick </v-icon>
                   </v-btn></v-col
                 >
-                <v-col
+
+                <v-col cols="2"
                   ><v-btn color="#FFFFFF" elevation="0">
                     <v-icon x-large> mdi-arrow-right-thick </v-icon>
                   </v-btn></v-col
@@ -42,9 +43,9 @@
           </v-window>
         </v-col>
 
-        <v-col cols="2">
+        <v-col cols="3">
           <h3 class="mb-2">Cevaplar</h3>
-          <v-row rows="8" no-gutters>
+          <v-row rows="4" no-gutters>
             <v-col cols="2">
               <p class="question-box">A</p>
             </v-col>
@@ -58,7 +59,7 @@
               <p class="question-box">A</p>
             </v-col>
           </v-row>
-          <v-row ows="8" no-gutters>
+          <v-row rows="4" no-gutters>
             <v-col cols="2">
               <p class="question-box">A</p>
             </v-col>
@@ -71,6 +72,32 @@
             <v-col cols="2">
               <p class="question-box">A</p>
             </v-col>
+          </v-row>
+          <v-row rows="4" no-gutters>
+            <section id="app" class="hero is-info is-fullheight is-bold">
+              <div class="hero-body">
+                <div class="container has-text-centered">
+                  <h2 class="title is-6">{{ title }}</h2>
+
+                  <div id="timer">
+                    <span id="minutes">{{ minutes }}</span>
+                    <span id="middle">:</span>
+                    <span id="seconds">{{ seconds }}</span>
+                  </div>
+
+                  <div id="buttons">
+                    <button
+                      id="start"
+                      class="button is-dark is-large"
+                      v-if="!timer"
+                      @click="startTimer"
+                    >
+                      <i class="far fa-play-circle"></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </section>
           </v-row>
         </v-col>
       </v-row>
@@ -82,8 +109,39 @@
 export default {
   data: () => {
     return {
-      step: 1,
+      timer: null,
+      totalTime: 25 * 60,
+      resetButton: false,
     };
+  },
+  methods: {
+    startTimer: function () {
+      this.timer = setInterval(() => this.countdown(), 1000);
+      this.resetButton = true;
+    },
+
+    padTime: function (time) {
+      return (time < 10 ? "0" : "") + time;
+    },
+    countdown: function () {
+      if (this.totalTime >= 1) {
+        this.totalTime--;
+      } else {
+        this.totalTime = 0;
+        this.resetTimer();
+      }
+    },
+  },
+
+  computed: {
+    minutes: function () {
+      const minutes = Math.floor(this.totalTime / 60);
+      return this.padTime(minutes);
+    },
+    seconds: function () {
+      const seconds = this.totalTime - this.minutes * 60;
+      return this.padTime(seconds);
+    },
   },
 };
 </script>
@@ -102,5 +160,11 @@ div {
 .question-box {
   border: 2px solid #bdbcbb;
   text-align: center;
+}
+
+#timer {
+  font-size: 50px;
+  line-height: 1;
+  margin-bottom: 40px;
 }
 </style>
